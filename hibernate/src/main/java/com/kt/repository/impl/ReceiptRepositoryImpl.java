9e768fc0845886f96fc26c8b93c1df9023524626
kt <tupham283200@gmail.com>
2024-07-06 16:28:55 +0700

    hgfjs

--- hibernate/src/main/java/com/kt/repository/impl/ReceiptRepositoryImpl.java
+++ hibernate/src/main/java/com/kt/repository/impl/ReceiptRepositoryImpl.java
@@ -4,10 +4,44 @@
  */
 package com.kt.repository.impl;
 
+import com.kt.hibernate.HibernatUtils;
+import com.kt.pojo.Cart;
+import com.kt.pojo.OrderDetail;
+import com.kt.pojo.SaleOrder;
+import static com.kt.pojo.User_.username;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
 /**
  *
  * @author admin
  */
 public class ReceiptRepositoryImpl {
+    private static final UserRepositoryImpl u = new UserRepositoryImpl();
+    private static final ProductReposityImpl pr = new ProductReposityImpl();
+    public void addReceipt(List<Cart> carts) {
+        if(carts != null )
+        {
+            try (Session s = HibernatUtils.getFactory().openSession()){
+               SaleOrder order = new SaleOrder();
+               order.setUserId(u.getUserByUserName("dhthanh"));
+               s.save(order);
+               
+               for (var c: carts){
+                   OrderDetail d = new OrderDetail();
+                   d.setUnitPrice(c.getUnitPrice());
+                   d.setProductId(pr.ProductById(c.getId()));
+                   d.setOrderId(order);
+                  
+                   s.save(d);
+               }
+               
+            
+            
+            
+        }
+        }
+    }
     
 }
